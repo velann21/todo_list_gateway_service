161c904f6d0765ab9aa95de5ba862a86f065fc9a
velann21 <velan@reynencourt.com>
2020-03-29 17:59:30 +0200

    Added the todo api

--- pkg/Orchestraters/grpc_orchestrator.go
+++ pkg/Orchestraters/grpc_orchestrator.go
@@ -1,6 +1,7 @@
 package Orchestraters
 import (
 	"context"
+	"github.com/sirupsen/logrus"
 	proto "github.com/velann21/todo_list_activity_manager/pkg/proto"
 )
 type GrpcActivityManagerService struct {
@@ -10,6 +11,7 @@ type GrpcActivityManagerService struct {
 func (am GrpcActivityManagerService) CreateTodoList(ctx context.Context, request *proto.CreateTodoListRequest)(*proto.CreateTodoListResponse,error){
 	resp, err := am.ActivityManager.CreateTodo(ctx, request)
 	if err!=nil{
+		logrus.Error("Somthing wrong:  ", err)
 		return nil, err
 	}
 	return resp, nil
--- pkg/controller/grpc_controllers.go
+++ pkg/controller/grpc_controllers.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"context"
+	"github.com/sirupsen/logrus"
 	"github.com/todo_list_gateway_service/pkg/Orchestraters"
 	"github.com/todo_list_gateway_service/pkg/entities/requests"
 	"github.com/todo_list_gateway_service/pkg/entities/responses"
@@ -13,9 +14,11 @@ type GrpcController struct {
 }
 
 func (grpcCtrl GrpcController) CreateTodoController(res http.ResponseWriter, req *http.Request){
+	logrus.Info("Inside CreateTodoController starts")
 	succesResp := responses.Response{}
 	reqBody, err := requests.PopulateCreateTodo(req.Body)
 	if err != nil{
+		logrus.Error("Something wrong:  ", err)
 		responses.HandleError(res, err)
 		return
 	}
@@ -23,9 +26,11 @@ func (grpcCtrl GrpcController) CreateTodoController(res http.ResponseWriter, req
 	service := Orchestraters.GrpcActivityManagerService{grpcCtrl.ActivityManager}
 	resp, err := service.CreateTodoList(context.Background(), reqBody)
 	if err != nil{
+		logrus.Error("Something wrong:  ", err)
 		responses.HandleError(res, err)
 		return
 	}
+	logrus.Info("Inside CreateTodoController Ends")
 	succesResp.NewActivityRegisterResp(&resp.Data[0].ID)
     succesResp.SendResponse(res, http.StatusOK)
 	return
