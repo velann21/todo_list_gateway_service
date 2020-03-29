18f71e3890d0b64394ce98532514442917f14a22
velann21 <velan@reynencourt.com>
2020-03-29 19:25:50 +0200

    Changed the version

--- pkg/Orchestraters/grpc_orchestrator.go
+++ pkg/Orchestraters/grpc_orchestrator.go
@@ -1,6 +1,7 @@
 package Orchestraters
 import (
 	"context"
+	"fmt"
 	"github.com/sirupsen/logrus"
 	proto "github.com/velann21/todo_list_activity_manager/pkg/proto"
 )
@@ -9,6 +10,7 @@ type GrpcActivityManagerService struct {
 }
 
 func (am GrpcActivityManagerService) CreateTodoList(ctx context.Context, request *proto.CreateTodoListRequest)(*proto.CreateTodoListResponse,error){
+	fmt.Println(am.ActivityManager)
 	resp, err := am.ActivityManager.CreateTodo(ctx, request)
 	if err!=nil{
 		logrus.Error("Somthing wrong:  ", err)
